deb53e984602e579a7a309986363d48a14cd8542
Prashant Swami <sprashant96624@gmail.com>
2025-07-12 04:48:25 +0000

    server check

---
+++ Back_End/index.js
@@ -0,0 +1,9 @@
+import express from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+
+dotenv.config()
+const app= express();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
