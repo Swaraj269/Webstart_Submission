2739307392d35e3e0ea3bc913a81d150dda4d4ae
Prashant Swami <sprashant96624@gmail.com>
2025-07-12 05:55:40 +0000

    database check and connection

--- Back_End/index.js
+++ Back_End/index.js
@@ -1,9 +1,35 @@
 import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
+import mysql from "mysql2"
 
 dotenv.config()
+const config = {
+  host: 'localhost',
+  user: process.env.DB_user,
+  password: process.env.DB_pass,
+  database: process.env.DB_name, 
+};
+
 const app= express();
 const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const testConnection = async () => {
+  try {
+    const connection = await mysql.createConnection(config);
+    console.log('Connected');
+
+    await connection.end();
+    console.log('Connection closed.');
+  } catch (err) {
+    console.error('Connection failed:', err.message);
+  }
+};
+
+testConnection();
 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
